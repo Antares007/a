efa7b75581f5df0ccbff2dab2f1a8eb2e79fb99f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-02-19 11:30:54 +0400

    n2

--- c.c
+++ c.c
@@ -1,159 +1,35 @@
-typedef void (*pith)(void *, const char *, ...);
-typedef void (*bark)(pith, void *);
+typedef void (*pith_t)(void *, const char *, void *, void *, void *);
+typedef void (*bark_t)(pith_t, void *);
 
-void lparen_r(int *o, const char *b) { *o = b[0] == '(' ? 1 : -1; }
-void lparen(pith o, void *s) {
-  const char *n = "R"
-                  "lparen";
-  o(s, n, lparen_r);
-}
-void rparen_r(int *o, const char *b) { *o = b[0] == ')' ? 1 : -1; }
-void rparen(pith o, void *s) {
-  const char *n = "R"
-                  "rparen";
-  o(s, n, rparen_r);
-}
-void plus_r(int *o, const char *b) { *o = b[0] == '+' ? 1 : -1; }
-void plus(pith o, void *s) {
-  const char *n = "R"
-                  "plus";
-  o(s, n, plus_r);
-}
-void mul_r(int *o, const char *b) { *o = b[0] == '*' ? 1 : -1; }
-void mul(pith o, void *s) {
-  const char *n = "R"
-                  "mul";
-  o(s, n, mul_r);
-}
-void dollar_r(int *o, const char *b) { *o = b[0] == '\0' ? 1 : -1; }
-void dollar(pith o, void *s) {
-  const char *n = "R"
-                  "dollar";
-  o(s, n, dollar_r);
-}
-void id_r(int *o, const char *b) {
-  int i = 0;
-  while ('a' <= b[i] && b[i] <= 'z')
-    ++i;
-  *o = i ? i : -1;
-}
-void id(pith o, void *s) {
-  const char *n = "R"
-                  "id";
-  o(s, n, id_r);
-}
+void noop() {}
+void id() {}
+void plus() {}
+void mul() {}
+void lparen() {}
+void rparen() {}
 
-void E(pith, void *);
-void T(pith, void *);
-void F(pith, void *);
+void expr(pith_t, void *);
+void term(pith_t, void *);
+void factor(pith_t, void *);
 
-void E0_l22(pith o, void *s) {
-  const char *n = "L"
-                  "E0"
-                  "_l22";
-  o(s, n, T, 0);
-}
-void E0_l12(pith o, void *s) {
-  const char *n = "L"
-                  "E0"
-                  "_l12";
-  o(s, n, plus, E0_l22);
-}
-void E0(pith o, void *s) {
-  const char *n = "L"
-                  "E0"
-                  "_l02";
-  o(s, n, E, E0_l12);
-}
-void E1(pith o, void *s) {
-  const char *n = "L"
-                  "E1"
-                  "_l00";
-  o(s, n, T, 0);
-}
-void E_l11(pith o, void *s) {
-  const char *n = "L"
-                  "E"
-                  "_l11";
-  o(s, n, E1, 0);
-}
-void E(pith o, void *s) {
-  const char *n = "L"
-                  "E"
-                  "_l01";
-  o(s, n, E0, E_l11);
-}
+void G24(pith_t o, void *s) { o(s, "G", G24, noop, 0); }
+void A25(pith_t o, void *s) { o(s, "A", A25, term, G24); }
+void G26(pith_t o, void *s) { o(s, "G", G26, noop, A25); }
+void A27(pith_t o, void *s) { o(s, "A", A27, term, G26); }
+void T28(pith_t o, void *s) { o(s, "T", T28, plus, A27); }
+void expr(pith_t o, void *s) { o(s, "expr", expr, expr, T28); }
 
-void T0_l22(pith o, void *s) {
-  const char *n = "L"
-                  "T0"
-                  "_l22";
-  o(s, n, F, 0);
-}
-void T0_l12(pith o, void *s) {
-  const char *n = "L"
-                  "T0"
-                  "_l12";
-  o(s, n, mul, T0_l22);
-}
-void T0(pith o, void *s) {
-  const char *n = "L"
-                  "T0"
-                  "_l02";
-  o(s, n, T, T0_l12);
-}
-void T1(pith o, void *s) {
-  const char *n = "L"
-                  "T1"
-                  "_l00";
-  o(s, n, F, 0);
-}
-void T_l11(pith o, void *s) {
-  const char *n = "L"
-                  "T"
-                  "_l11";
-  o(s, n, T1, 0);
-}
-void T(pith o, void *s) {
-  const char *n = "L"
-                  "T"
-                  "_l01";
-  o(s, n, T0, T_l11);
-}
+void G30(pith_t o, void *s) { o(s, "G", G30, noop, 0); }
+void A31(pith_t o, void *s) { o(s, "A", A31, factor, G30); }
+void G32(pith_t o, void *s) { o(s, "G", G32, noop, A31); }
+void A33(pith_t o, void *s) { o(s, "A", A33, factor, G32); }
+void T34(pith_t o, void *s) { o(s, "T", T34, mul, A33); }
+void term(pith_t o, void *s) { o(s, "term", term, term, T34); }
 
-void F0_l22(pith o, void *s) {
-  const char *n = "L"
-                  "F0"
-                  "_l22";
-  o(s, n, rparen, 0);
-}
-void F0_l12(pith o, void *s) {
-  const char *n = "L"
-                  "F0"
-                  "_l12";
-  o(s, n, E, F0_l22);
-}
-void F0(pith o, void *s) {
-  const char *n = "L"
-                  "F0"
-                  "_l02";
-  o(s, n, lparen, F0_l12);
-}
-void F1(pith o, void *s) {
-  const char *n = "L"
-                  "F1"
-                  "_l00";
-  o(s, n, id, 0);
-}
-void F_l11(pith o, void *s) {
-  const char *n = "L"
-                  "F"
-                  "_l11";
-  o(s, n, F1, 0);
-}
-void F(pith o, void *s) {
-  const char *n = "L"
-                  "F"
-                  "_l01";
-  o(s, n, F0, F_l11);
-}
+void G36(pith_t o, void *s) { o(s, "G", G36, noop, 0); }
+void T37(pith_t o, void *s) { o(s, "T", T37, id, G36); }
+void G38(pith_t o, void *s) { o(s, "G", G38, noop, T37); }
+void T39(pith_t o, void *s) { o(s, "T", T39, rparen, G38); }
+void A40(pith_t o, void *s) { o(s, "A", A40, expr, T39); }
+void T41(pith_t o, void *s) { o(s, "T", T41, lparen, A40); }
+void factor(pith_t o, void *s) { o(s, "factor", factor, factor, T41); }
---
+++ n2.c
@@ -0,0 +1,42 @@
+typedef void (*pith_t)(void *, const char *, void *, void *, void *);
+typedef void (*bark_t)(pith_t, void *);
+
+void noop() {}
+void id() {}
+void plus() {}
+void mul() {}
+void lparen() {}
+void rparen() {}
+
+void expr(pith_t, void *);
+void term(pith_t, void *);
+void fact(pith_t, void *);
+/* clang-format off */
+#define CONCAT_IMP(a,b) a##b
+#define CONCAT(a,b) CONCAT_IMP(a,b)
+#define N(b, a, n)                                                             \
+  void n(pith_t o, void *s) { o(s, #n, n, a, b); }
+#define G CONCAT(G, __LINE__ )
+#define A CONCAT(A, __LINE__ )
+#define T CONCAT(T, __LINE__ )
+#define C CONCAT(C, __LINE__ )
+
+N(0, noop   , G)N(G, 
+     term   , A)N(A, 
+     noop   , G)N(G, 
+     term   , A)N(A, 
+     plus   , T)N(T,
+     expr   , expr)
+N(0, noop   , G)N(G,
+     fact   , A)N(A,
+     noop   , G)N(G,
+     fact   , A)N(A,
+     mul    , T)N(T,
+     term   , term)
+N(0, noop   , G)N(G,
+     id     , T)N(T,
+     noop   , G)N(G,
+     rparen , T)N(T,
+     expr   , A)N(A,
+     lparen , T)N(T,
+     fact   , fact)
