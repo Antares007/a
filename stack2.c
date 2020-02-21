919ababea499141b7fe233e599aab02103eaa358
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-02-21 11:39:32 +0400

    add bark join macro

--- stack2.c
+++ stack2.c
@@ -11,47 +11,40 @@ void b_bark(void *b, b_pith_t o, int a) {
 }
 #define CONCAT_IMP(a, b) a##b
 #define CONCAT(a, b) CONCAT_IMP(a, b)
-#define CONCAT3_IMP(a, b, c) a##b##c
-#define CONCAT3(a, b, c) CONCAT3_IMP(a, b, c)
-#define CONCAT6_IMP(a, b, c, d, e, f) a##b##c##d##e##f
-#define CONCAT6(a, b, c, d, e, f) CONCAT6_IMP(a, b, c, d, e, f)
-
-#define JOIN(l_bark, l_o_type, l_a_type, r_bark, r_o_type, r_a_type)           \
+#define JOIN(name, l_bark, l_o_type, l_a_type, r_bark, r_o_type, r_a_type)     \
   JOIN_(l_bark, l_o_type, l_a_type, r_bark, r_o_type, r_a_type,                \
-        CONCAT6(l_bark, _, r_bark, _b_, __LINE__, _t),                         \
-        CONCAT3(l_bark, _, r_bark))
-#define JOIN_(l_bark, l_o_type, l_a_type, r_bark, r_o_type, r_a_type, b_t,     \
-              bark)                                                            \
+        CONCAT(name, _a_t), name, CONCAT(name, _o_t), CONCAT(name, _l_pith),   \
+        CONCAT(name, _r_pith))
+#define JOIN_(l_bark, l_o_type, l_a_type, r_bark, r_o_type, r_a_type, a_t,     \
+              bark, o_t, l_pith, r_pith)                                       \
   typedef struct {                                                             \
     l_a_type l;                                                                \
     r_a_type r;                                                                \
   } a_t;                                                                       \
   typedef struct {                                                             \
-    void (*o)(void *, l_o_type, r_o_type);                                     \
-    void *b;                                                                   \
-    r_a_type a;                                                                \
     l_o_type l;                                                                \
-  } b_t;                                                                       \
-  static void r_pith(void *b_, r_o_type r) {                                   \
-    b_t *b = b_;                                                               \
-    b->o(b->b, b->l, r);                                                       \
+    r_o_type r;                                                                \
+  } o_t;                                                                       \
+  static void r_pith(void *ba, r_o_type r) {                                   \
+    void **b = ba;                                                             \
+    void (*o)(void *, o_t) = b[0];                                             \
+    o(b[1], (o_t){.l = *((l_o_type *)b[2]), .r = r});                          \
   }                                                                            \
-  static void l_pith(void *b_, l_o_type l) {                                   \
-    b_t *b = b_;                                                               \
-    b->l = l;                                                                  \
-    r_bark(b, r_pith, b->a);                                                   \
+  static void l_pith(void *ba, l_o_type l) {                                   \
+    void **b = ba;                                                             \
+    r_bark((void *[]){b[0], b[1], &l}, r_pith, *(r_a_type *)b[2]);             \
   }                                                                            \
-  void bark(void *b_, void (*o)(void *, l_o_type, r_o_type), a_t a) {          \
-    b_t b = {.o = o, .b = b_, .a = a.r};                                       \
-    l_bark(&b, l_pith, a.l);                                                   \
+  void bark(void *b, void (*o)(void *, o_t), a_t a) {                          \
+    l_bark((void *[]){o, b, &a.r}, l_pith, a.l);                               \
   }
-JOIN(a_bark, const char *, const char *, //
-     b_bark, int, int)
+JOIN(a_bark_b_bark,                      //
+     a_bark, const char *, const char *, //
+     b_bark, int, int)                   //
 
 #include <stdio.h>
-void ab_pith(void *s, const char *a, int b) { //
-  printf("%p %s %d\n", s, a, b);
+static void ab_pith(void *s, a_bark_b_bark_o_t v) { //
+  printf("%p %s %d\n", s, v.l, v.r);
 }
 int main() { //
-  a_bark_b_bark(0, ab_pith, (a_t){"!", 39});
+  a_bark_b_bark(0, ab_pith, (a_bark_b_bark_a_t){"!", 39});
 }
