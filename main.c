0a4b80b64e716151f32ec8ff705ea2f688bca040
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-02-21 21:17:19 +0400

    sync

--- main.c
+++ main.c
@@ -6,25 +6,17 @@
 
 typedef void (*bark)(void *, pith_t, void *);
 
-bool contains(void **list, void *value) {
-  for (; list; list = list[1])
-    if (list[0] == value)
-      return true;
-  return false;
-}
+inline bool contains(void **list, void *value);
+inline void ident_print(int ident, const char *n);
 void traverse_pith(void *b_, const char *n, void *h, void *t) {
   void **b = b_;
   int *ident = b[0];
   void **path = b[1];
-  for (int i = *ident; i > 0; i--)
-    printf(". . ");
-  printf("%s\n", n);
+  ident_print(*ident, n);
   if (n[0] == 'A') {
     b_ = (void *[]){b[0], (void *[]){h, path}};
-    (*ident)++;
     if (!contains(path, h))
-      ((bark)h)(b_, traverse_pith, 0);
-    (*ident)--;
+      ((*ident)++, ((bark)h)(b_, traverse_pith, 0), (*ident)--);
   }
   if (t)
     ((bark)t)(b_, traverse_pith, 0);
@@ -33,7 +25,20 @@ void print(bark g) {
   int ident = 0;
   g((void *[]){&ident, (void *[]){g, 0}}, traverse_pith, 0);
 }
+
 int main() {
-  print(g41_32);
+  print(g41_A_expr);
   return 9;
+}
+
+void ident_print(int ident, const char *n) {
+  while (ident--)
+    printf(". . ");
+  printf("%s\n", n);
+}
+bool contains(void **list, void *value) {
+  for (; list; list = list[1])
+    if (list[0] == value)
+      return true;
+  return false;
 }
