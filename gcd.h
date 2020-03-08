d298b368e494609ef27b7be2323d130ea7e7183f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-03-08 15:43:18 +0400

    sync

---
+++ gcd.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "t.h"
+void _1() {}
+void _2() {}
+void _3() {}
+void _4() {}
+void _5() {}
+void _6() {}
+void _7() {}
+// clang-format off
+  N(0, _7,  
+L)N(L,  o,
+L)N(L, _6,
+L)N(L,  o,
+L)N(L,  O, O)
+  N(0, _5,  
+L)N(L,  b,
+L)N(L, _4,
+L)N(L,  b,
+L)N(L,  B, B)
+  N(0, _3,  
+L)N(L,  a,
+L)N(L, _2,
+L)N(L,  a,
+L)N(L,  A, A)
+  N(0, _1,
+L)N(L,  O,
+L)N(L,  B,
+L)N(L,  A, S)
+
+// S->ABO
