a99e79910baa8363f9c11d6613a19424ae45d498
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-03-10 17:09:59 +0400

    sync

--- c.c
+++ c.c
@@ -1,17 +1,4 @@
-# 1 "gll.h"
-# 1 "<built-in>" 1
-# 1 "<built-in>" 3
-# 341 "<built-in>" 3
-# 1 "<command line>" 1
-# 1 "<built-in>" 2
-# 1 "gll.h" 2
-
-# 1 "./t.h" 1
-
-# 1 "./n.h" 1
-
-typedef void (*pith_t)(void *, const char *, void *, void *);
-# 3 "./t.h" 2
+#include "n.h"
 
 void i(int *o, const char *b) {
   int i = 0;
@@ -29,13 +16,20 @@ void b(int *o, const char *b) { *o = *b == 'b' ? 1 : -1; }
 void c(int *o, const char *b) { *o = *b == 'c' ? 1 : -1; }
 void d(int *o, const char *b) { *o = *b == 'd' ? 1 : -1; }
 void eps(int *o, const char *b) { *o = 0; }
-# 3 "gll.h" 2
 
 void S(pith_t, void *);
-void _3() {}
-void _2() {}
-void _1() {}
+void A(pith_t, void *);
 
+void _3(){};
+void _2(){};
+void _1(){};
 
+void n13(pith_t o, void *bs) { o(bs, "_3", _3, 0); }
+void n14(pith_t o, void *bs) { o(bs, "b", b, n13); }
+void n15(pith_t o, void *bs) { o(bs, "_2", _2, n14); }
+void n16(pith_t o, void *bs) { o(bs, "A", A, n15); }
+void A(pith_t o, void *bs) { o(bs, "a", a, n16); }
 
-  void n12(pith_t o, void *bs) { o(bs, "_2", _2, 0); }void n13(pith_t o, void *bs) { o(bs, "a", a, n12); }void n14(pith_t o, void *bs) { o(bs, "S", S, n13); }void n15(pith_t o, void *bs) { o(bs, "_1", _1, n14); }void S(pith_t o, void *bs) { o(bs, "a", a, n15); }
+void n19(pith_t o, void *bs) { o(bs, "_1", _1, 0); }
+void n20(pith_t o, void *bs) { o(bs, "A", A, n19); }
+void S(pith_t o, void *bs) { o(bs, "A", A, n20); }
---
+++ gc.c
@@ -0,0 +1,136 @@
+#include "n.h"
+/*
+6.4.1 Keywords
+Semantics
+The above tokens (case sensitive) are reserved (in translation phases 7 and 8)
+for use as keywords, and shall not be used otherwise. The keyword _Imaginary is
+reserved for specifying imaginary types.  */
+static const char kw_auto[] = "auto";
+static const char kw_break[] = "break";
+static const char kw_case[] = "case";
+static const char kw_char[] = "char";
+static const char kw_const[] = "const";
+static const char kw_continue[] = "continue";
+static const char kw_default[] = "default";
+static const char kw_do[] = "do";
+static const char kw_double[] = "double";
+static const char kw_else[] = "else";
+static const char kw_enum[] = "enum";
+static const char kw_extern[] = "extern";
+static const char kw_float[] = "float";
+static const char kw_for[] = "for";
+static const char kw_goto[] = "goto";
+static const char kw_Star[] = "∗";
+static const char kw_if[] = "if";
+static const char kw_inline[] = "inline";
+static const char kw_int[] = "int";
+static const char kw_long[] = "long";
+static const char kw_register[] = "register";
+static const char kw_restrict[] = "restrict";
+static const char kw_return[] = "return";
+static const char kw_short[] = "short";
+static const char kw_signed[] = "signed";
+static const char kw_sizeof[] = "sizeof";
+static const char kw_static[] = "static";
+static const char kw_struct[] = "struct";
+static const char kw_switch[] = "switch";
+static const char kw_typedef[] = "typedef";
+static const char kw_union[] = "union";
+static const char kw_unsigned[] = "unsigned";
+static const char kw_void[] = "void";
+static const char kw_volatile[] = "volatile";
+static const char kw_while[] = "while";
+static const char kw__Alignas[] = "_Alignas";
+static const char kw__Alignof[] = "_Alignof";
+static const char kw__Atomic[] = "_Atomic";
+static const char kw__Bool[] = "_Bool";
+static const char kw__Complex[] = "_Complex";
+static const char kw__Generic[] = "_Generic";
+static const char kw__Imaginary[] = "_Imaginary";
+static const char kw__Noreturn[] = "_Noreturn";
+static const char kw__Static_assert[] = "_Static_assert";
+static const char kw__Thread_local[] = "_Thread_local";
+static const char *keyword[] = // one of
+    {kw_auto,         kw_break,      kw_case,      kw_char,
+     kw_const,        kw_continue,   kw_default,   kw_do,
+     kw_double,       kw_else,       kw_enum,      kw_extern,
+     kw_float,        kw_for,        kw_goto,      kw_Star,
+     kw_if,           kw_inline,     kw_int,       kw_long,
+     kw_register,     kw_restrict,   kw_return,    kw_short,
+     kw_signed,       kw_sizeof,     kw_static,    kw_struct,
+     kw_switch,       kw_typedef,    kw_union,     kw_unsigned,
+     kw_void,         kw_volatile,   kw_while,     kw__Alignas,
+     kw__Alignof,     kw__Atomic,    kw__Bool,     kw__Complex,
+     kw__Generic,     kw__Imaginary, kw__Noreturn, kw__Static_assert,
+     kw__Thread_local};
+
+/* nondigit: one of
+_ a b c d e f g h i j k l yym
+  A B C D E F G H I J K L M
+  n o p q r s t u v w x y z
+  N O P Q R S T U V W X Y Z */
+void nondigit(int *o, const char *b) {
+  *o = ('a' <= *b && *b <= 'z') || *b == '_' || ('A' <= *b && *b <= 'Z') ? 1
+                                                                         : -1;
+}
+
+/* digit: one of 013456789 */
+void digit(int *o, const char *b) { *o = ('0' <= *b && *b <= '9') ? 1 : -1; }
+
+void _() {}
+void UniversalCharacterName(pith_t o, void *vs[2]);
+// clang-format off
+/*6.4.3 Universal character names
+1) Syntax
+*/
+/* universal-character-name:
+        \u hex-quad
+        \U hex-quad hex-quad*/
+/* hex-quad:
+        hexadecimal-digit hexadecimal-digit
+        hexadecimal-digit hexadecimal-digit*/
+/*
+6.4.2 Identifiers
+1) Syntax
+*/
+/* identifier-nondigit:
+        nondigit
+        universal-character-name*/
+  N(0, _,
+L)N(L, nondigit,
+L)N(L, _,
+L)N(L, UniversalCharacterName,
+       IdentifierNondigit);
+
+/* identifier:
+        identifier-nondigit
+        identifier identifier-nondigit
+        identifier digit */
+void Identifier(pith_t o, void*vs);
+  N(0,      _,
+L)N(L,      IdentifierNondigit,
+L)N(L,      _,
+L)N(L,      IdentifierNondigit,
+L)N(L,      Identifier,
+L)N(L,      _,
+L)N(L,      digit,
+L)N(L,      Identifier,
+            Identifier);
+/*
+
+Semantics
+2) An identifier is a sequence of nondigit characters (including the underscore
+_, the lowercase and uppercase Latin letters, and other characters) and digits,
+which designates one or more entities as described in 6.2.1. Lowercase and
+uppercase letters are distinct. There is no specific limit on the maximum length
+of an identifier.
+3) Each universal character name in an identifier shall
+designate a character whose encoding in ISO/IEC 10646 falls into one of the
+ranges specified in D.1. 71) The initial character shall not be a universal
+character name designating a character whose encoding falls into one of the
+ranges specified in D.2. An implementation may allow multibyte characters that
+are not part of the basic source character set to appear in identifiers; which
+characters and their correspondence to universal character names is
+implementation-defined.
+*/
+int main() { (void)keyword; }
